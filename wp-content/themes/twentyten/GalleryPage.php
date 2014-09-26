ecbd8952bb7678702a67e7d057b1387c89c824ca
Adam Pearce <adam.r.pearce@gmail.com>
2014-09-25 22:11:09 -0400

    adds images

---
+++ wp-content/themes/twentyten/GalleryPage.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: GalleryPage
+ *
+ * A custom page template without sidebar.
+ *
+ * The "Template Name:" bit above allows this to be selectable
+ * from a dropdown menu on the edit page screen.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+			<?php
+			/* Run the loop to output the page.
+			 * If you want to overload this in a child theme then include a file
+			 * called loop-page.php and that will be used instead.
+			 */
+			get_template_part( 'loop', 'page' );
+			?>
+			<?php 
+				if (function_exists('WPPortfolio_getAllPortfolioAsHTML')) {
+					echo WPPortfolio_getAllPortfolioAsHTML('1,3');
+			}
+			?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
