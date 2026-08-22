00d908ce4bde69c268b2269734863e43707f67a0
Adam Pearce <1wheel@gmail.com>
2026-08-22 19:09:13 -0400

    podcast sketch: Sonnet guest sweep (+928 eps), EK final pass, 538 Politics 2016-2018 backfill (+250); 9,186 episodes

--- sketches/podcast/init.js
+++ sketches/podcast/init.js
@@ -3,7 +3,7 @@ window.init = function(){
   drawTours(window.episodes)
 }
 
-window.DATA_V = '20260822j'   // bump when episodes.json changes so browsers refetch
+window.DATA_V = '20260822k'   // bump when episodes.json changes so browsers refetch
 fetch('episodes.json?v=' + window.DATA_V)
   .then(r => r.json())
   .then(eps => {
